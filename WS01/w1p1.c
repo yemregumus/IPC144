a11f33b12d6522d3400c49af5e5c6f7e6f80d8b9
Yunus Emre Gumus <ygumus@myseneca.ca>
2022-04-26 15:20:37 -0400

    Add files via upload

---
+++ WS01/w1p1.c
@@ -0,0 +1,18 @@
+//Yunus Emre Gumus
+//150331197
+//ygumus@myseneca.ca
+//NEE
+
+#include <stdio.h>
+
+int main(void)
+{
+	printf("*** Welcome to Seneca's C Programming Course ***\n");
+	printf("+-------------------------------+\n");
+	printf("| Introduction to C Programming |\n");
+	printf("+-------------------------------+\n");
+	printf("Program written by:\n"); 
+	printf("Yunus Emre Gumus\n");
+
+	return 0;
+}
