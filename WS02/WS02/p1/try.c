295abb10d25040565621dda3da7785bc1b00c45e
Yunus Emre Gumus <ygumus@myseneca.ca>
2022-04-26 15:21:34 -0400

    Add files via upload

---
+++ WS02/WS02/p1/try.c
@@ -0,0 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void)
+{
+	int number1, number2, add, subtract, multiply, divide;
+
+	number1 = 44;
+	number2 = 2353;
+	
+	add = number1 + number2;
+
+	printf("Total: %d\n", add);
+
+	return 0;
+}
